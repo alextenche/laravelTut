414b45bf84107bc79b2d1e5e925ce87451b15485
alexTenche <alex.tenche@gmail.com>
2015-02-06 11:54:04 +0200

    working on database CRUD

--- lara3/app/controllers/PostController.php
+++ lara3/app/controllers/PostController.php
@@ -4,11 +4,13 @@ class PostController extends BaseController
 {
 	
 	public function listing(){
-		return View::make('post.listing');
+		$posts = Post::where('title', 'LIKE', '%post%')->take(1)->skip(1)->get();
+		return View::make('post.listing', compact('posts') );
 	}
 
 	public function single($id){
-		return View::make('post.single')->with('id', $id);
+		$post = Post::find($id);
+		return View::make('post.single')->with('post', $post);
 	}
 
 	public function update($id){
---
+++ lara3/app/views/post/single.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.default')
+
+@section('content')
+	<h1>{{{ $post->title }}}</h1>
+@stop
