2309a312e5183a0b77588d4d7a33453b4c2d88e1
Alex Tenche <alex.tenche@gmail.com>
2015-10-17 09:36:48 +0300

    added update to CRUD

--- LaravelBasics/app/views/todos/index.blade.php
+++ LaravelBasics/app/views/todos/index.blade.php
@@ -3,10 +3,14 @@
 
   <h2> All Todos List </h2>
 
-  <ul>
     @foreach($todo_lists as $list)
-      <li> {{{ $list->name }}} </li>
+       <h4> {{ link_to_route('todos.show', $list->name, [$list->id]) }} </h4>
+       <ul class="no-bullet button-group">
+         <li>{{ link_to_route('todos.edit', 'edit', [$list->id], ['class' => 'tiny button'] ) }}</li>
+
+       </ul>
     @endforeach
-  </ul>
+
+    <p> {{ link_to_route('todos.create', 'Create New List', null, ['class' => 'success button']) }} </p>
 
 @stop
--- LaravelBasics/app/views/todos/show.blade.php
+++ LaravelBasics/app/views/todos/show.blade.php
@@ -1,6 +1,9 @@
 @extends('layouts.main')
 @section('content')
 
-  <h2> {{{ $list->name }}} </h2>
+  <div class="large-12 columns">
+    <h2> {{{ $list->name }}} </h2>
+    <p> {{ link_to_route('todos.index', 'back') }} </p>
+  </div>
 
 @stop
